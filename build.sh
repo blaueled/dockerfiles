11355ded343baecadee050d6b15c2a5b252f871e
Alex Rhea <alex.rhea@gmail.com>
2017-07-14 10:21:24 -0400

    aligning hashicorp image build processes

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ docker image build -t arhea/terraform:latest -f ./terraform/Dockerfile ./terrafo
 docker image build -t arhea/ansible:latest -f ./ansible/Dockerfile ./ansible
 docker image build -t arhea/awscli:latest -f ./awscli/Dockerfile ./awscli
 docker image build -t arhea/papertrail:latest -f ./papertrail/Dockerfile ./papertrail
+docker image build -t arhea/gcloud:latest -f ./gcloud/Dockerfile ./gcloud
