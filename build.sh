cbc1f41b91500e9a56f4e0edb39a926b7d6717b2
Alex Rhea <alex.rhea@gmail.com>
2017-07-14 08:28:06 -0400

    gcloud

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 docker image build -t arhea/packer:latest -f ./packer/Dockerfile ./packer
 docker image build -t arhea/terraform:latest -f ./terraform/Dockerfile ./terraform
+docker image build -t arhea/ansible:latest -f ./ansible/Dockerfile ./ansible
+docker image build -t arhea/awscli:latest -f ./awscli/Dockerfile ./awscli
+docker image build -t arhea/papertrail:latest -f ./papertrail/Dockerfile ./papertrail
