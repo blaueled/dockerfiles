d4810179c5ffb1c5819fa582f06962670c33f219
Alex Rhea <alex.rhea@gmail.com>
2017-07-16 16:48:07 -0400

    updating file permissions

--- push.sh
+++ push.sh
@@ -2,5 +2,9 @@
 
 docker login
 
-docker push arhea/packer:latest
-docker push arhea/terraform:latest
+docker image pull arhea/packer:latest
+docker image pull arhea/terraform:latest
+docker image pull arhea/ansible:latest
+docker image pull arhea/awscli:latest
+docker image pull arhea/papertrail:latest
+docker image pull arhea/gcloud:latest
